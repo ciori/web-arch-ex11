cb4c687760dfcc95f1171553a13da442114d1b88
ciori <mirkomarchiori94@gmail.com>
2018-12-13 16:16:27 +0100

    Continued Setup,Show

--- src/main/java/it/unitn/mirkomarchiori1/webarchex11/controller/ShowController.java
+++ src/main/java/it/unitn/mirkomarchiori1/webarchex11/controller/ShowController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ShowController {
@@ -20,6 +22,13 @@ public class ShowController {
     @GetMapping("/showData")
     public String showData(Model model) {
         String resultAsHtml = showService.showData();
+        model.addAttribute("resultAsHtml", resultAsHtml);
+        return "showResult";
+    }
+
+    @PostMapping("/showStudentsInCourse")
+    public String showStudentsInCourse(@RequestParam("name") String name, Model model) {
+        String resultAsHtml = showService.showStudentsInCourse(name);
         model.addAttribute("resultAsHtml", resultAsHtml);
         return "showResult";
     }
--- src/main/java/it/unitn/mirkomarchiori1/webarchex11/service/ShowService.java
+++ src/main/java/it/unitn/mirkomarchiori1/webarchex11/service/ShowService.java
@@ -1,13 +1,17 @@
 package it.unitn.mirkomarchiori1.webarchex11.service;
 
 import it.unitn.mirkomarchiori1.webarchex11.model.Course;
+import it.unitn.mirkomarchiori1.webarchex11.model.Exam;
 import it.unitn.mirkomarchiori1.webarchex11.model.Professor;
 import it.unitn.mirkomarchiori1.webarchex11.model.Student;
 import it.unitn.mirkomarchiori1.webarchex11.repository.CourseRepository;
+import it.unitn.mirkomarchiori1.webarchex11.repository.ExamRepository;
 import it.unitn.mirkomarchiori1.webarchex11.repository.ProfessorRepository;
 import it.unitn.mirkomarchiori1.webarchex11.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ShowService {
@@ -18,23 +22,25 @@ public class ShowService {
     private ProfessorRepository professorRepository;
     @Autowired
     private CourseRepository courseRepository;
+    @Autowired
+    private ExamRepository examRepository;
 
     public String showData() {
         String resString = "<div>";
         resString = resString + "<h3>ALL STORED DATA</h3>";
 
         resString = resString + "<h5>STUDENTS:</h5>";
-        for(Student student: studentRepository.findAll()) {
+        for (Student student: studentRepository.findAll()) {
             resString = resString + "<p>" + student.getName() + " " + student.getSurname() + " " + student.getMatriculationNumber() + "</p>";
         }
 
         resString = resString + "<h5>PROFESSORS:</h5>";
-        for(Professor professor: professorRepository.findAll()) {
+        for (Professor professor: professorRepository.findAll()) {
             resString = resString + "<p>" + professor.getName() + " " + professor.getSurname() + "</p>";
         }
 
         resString = resString + "<h5>COURSES:</h5>";
-        for(Course course: courseRepository.findAll()) {
+        for (Course course: courseRepository.findAll()) {
             resString = resString + "<p>" + course.getName() + "</p>";
             if (course.getProfessor() != null) {
                 resString = resString + "<p style='text-indent: 2em;'>teacher:" + course.getProfessor().getName() + "</p>";
@@ -44,16 +50,44 @@ public class ShowService {
             }
             resString = resString + "<p style='text-indent: 2em;'>students:</p>";
             for (Student student: course.getStudents()) {
-                resString = resString + "<p style='text-indent: 4em;'>teacher: " + course.getProfessor().getName() +
+                resString = resString + "<p style='text-indent: 4em;'>" + student.getName() +
                         " " + student.getSurname() + " " + student.getMatriculationNumber() + "</p>";
             }
-            resString = resString + "<p style='text-indent: 2em;'>exam on: ";
-            if (course.getExam() != null) {
-                resString = resString + "" + course.getExam().getDate().toString();
-            }
-            resString = resString + "</p>";
         }
 
+        resString = resString + "<h5>EXAMS:</h5>";
+        for (Exam exam: examRepository.findAll()) {
+            resString = resString + "<p><b>" + exam.getName() + "</b> on " + exam.getDate().toString() + " with enrolled students - grades:</p>";
+            List<Student> students = exam.getStudents();
+            List<Integer> grades = exam.getGrades();
+            if (students.size() == 0) {
+                resString = resString + "<p style='text-indent: 2em;'>No Students registered to this Exam!</p>";
+            }
+            for (int i=0;i<students.size();i++) {
+                resString = resString + "<p style='text-indent: 2em;'>" + students.get(i).getName() +
+                        " " + students.get(i).getSurname() + " " + students.get(i).getMatriculationNumber() + " - " + grades.get(i) + "</p>";
+            }
+        }
+
+        resString = resString + "</div>";
+        return resString;
+    }
+
+    public String showStudentsInCourse(String name) {
+        if (courseRepository.findByName(name).size() == 0) {
+            return "<div><p>No available Course named <b>" + name + "</b> found!<p></div>";
+        }
+        String resString = "<div>";
+        resString = resString + "<h3>LIST OF ENROLLED STUDENTS IN THE COURSE: <b>" + name + "</b></h3>";
+        if (courseRepository.findByName(name).get(0).getStudents().size() == 0) {
+            resString = resString + "<p style='text-indent: 2em;'>No Students enrolled in this Course!</p>";
+        }
+        else {
+            for (Student student: courseRepository.findByName(name).get(0).getStudents()) {
+                resString = resString + "<p style='text-indent: 2em;'>" + student.getName() +
+                        " " + student.getSurname() + " " + student.getMatriculationNumber() + "</p>";
+            }
+        }
         resString = resString + "</div>";
         return resString;
     }
